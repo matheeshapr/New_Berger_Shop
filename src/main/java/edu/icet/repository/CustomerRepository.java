d11131427a7200d79dec91951823ed2caaeb9012
matheeshapr <matheeshap62@gmail.com>
2025-11-26 09:31:07 +0530

    Configures customer repository for creation REST endpoint.

--- src/main/java/edu/icet/repository/CustomerRepository.java
+++ src/main/java/edu/icet/repository/CustomerRepository.java
@@ -1,4 +1,9 @@
 package edu.icet.repository;
 
-public interface CustomerRepository {
+import edu.icet.model.entity.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CustomerRepository extends JpaRepository<Customer,Integer> {
 }

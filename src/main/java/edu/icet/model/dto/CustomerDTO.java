51b99fbf360c43e715dd2f8c506d786537e0cb64
matheeshapr <matheeshap62@gmail.com>
2025-11-26 09:30:27 +0530

    Adds CustomerDTO for customer creation REST endpoint.

--- src/main/java/edu/icet/model/dto/CustomerDTO.java
+++ src/main/java/edu/icet/model/dto/CustomerDTO.java
@@ -1,4 +1,17 @@
 package edu.icet.model.dto;
 
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
 public class CustomerDTO {
+
+    private int id;
+    private String name;
+    private String address;
+    private double ssalary;
 }

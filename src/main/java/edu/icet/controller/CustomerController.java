883c1020d21b2e614fba028797b98b9a062bdb25
matheeshapr <matheeshap62@gmail.com>
2025-11-26 09:29:50 +0530

    Adds customer creation REST endpoint.

--- src/main/java/edu/icet/controller/CustomerController.java
+++ src/main/java/edu/icet/controller/CustomerController.java
@@ -1,4 +1,23 @@
 package edu.icet.controller;
 
+import edu.icet.model.entity.Customer;
+import edu.icet.service.CustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("customers")
 public class CustomerController {
+
+    @Autowired
+    CustomerService customerService;
+
+    @PostMapping("addcustomer")
+    public void addCustomer(@RequestBody Customer customer) {
+        customerService.add(customer);
+    }
+
 }

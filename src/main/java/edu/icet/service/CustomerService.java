0b6e2e8afa97382279ab07518935f00df9aeeb6e
matheeshapr <matheeshap62@gmail.com>
2025-11-26 09:31:44 +0530

    Configures customer service to persist new customers via repository.

--- src/main/java/edu/icet/service/CustomerService.java
+++ src/main/java/edu/icet/service/CustomerService.java
@@ -1,4 +1,17 @@
 package edu.icet.service;
 
+import edu.icet.model.entity.Customer;
+import edu.icet.repository.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class CustomerService {
+
+    @Autowired
+    CustomerRepository customerRepository;
+
+    public void add(Customer customer) {
+        customerRepository.save(customer);
+    }
 }

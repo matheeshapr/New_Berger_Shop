667b3128b1b62b710c30f5e3fa8470cb2deb120c
matheeshapr <matheeshap62@gmail.com>
2025-12-01 11:01:59 +0530

    Enables ItemRepository for Item entity JPA persistence.

--- src/main/java/edu/icet/repository/ItemRepository.java
+++ src/main/java/edu/icet/repository/ItemRepository.java
@@ -1,4 +1,11 @@
 package edu.icet.repository;
 
-public interface ItemRepository {
+import edu.icet.model.dto.ItemDTO;
+import edu.icet.model.entity.Item;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ItemRepository extends JpaRepository<Item,Integer> {
+
 }

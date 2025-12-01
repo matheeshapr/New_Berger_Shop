6b5b74e42da9091477f65c063d804129f58f73c5
matheeshapr <matheeshap62@gmail.com>
2025-12-01 11:02:14 +0530

    Implements ItemService to handle item persistence with ItemRepository

--- src/main/java/edu/icet/service/ItemService.java
+++ src/main/java/edu/icet/service/ItemService.java
@@ -1,4 +1,17 @@
 package edu.icet.service;
 
+import edu.icet.model.entity.Item;
+import edu.icet.repository.ItemRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class ItemService {
+
+    @Autowired
+    ItemRepository itemRepository;
+
+    public void add(Item item) {
+        itemRepository.save(item);
+    }
 }

a810344df6386ff4ab4a4cae102349888463ea0d
matheeshapr <matheeshap62@gmail.com>
2025-12-01 11:01:29 +0530

    Creates Item controller and `addItem` endpoint for persistence.

--- src/main/java/edu/icet/controller/ItemContoller.java
+++ src/main/java/edu/icet/controller/ItemContoller.java
@@ -1,4 +1,26 @@
 package edu.icet.controller;
 
+import edu.icet.model.entity.Item;
+import edu.icet.service.ItemService;
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("items")
 public class ItemContoller {
+
+    @Autowired
+    ItemService itemService;
+
+    @PostMapping("additem")
+    public void addItem(@RequestBody Item item){
+        itemService.add(item);
+    }
+
+//    @GetMapping("getAll")
+//    public String getAll(){
+//        return "Matheesha";
+//    }
+
 }

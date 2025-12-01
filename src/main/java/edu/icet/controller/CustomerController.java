461beeaacb22ad1b3adb0f2eb1f74313599c2715
matheeshapr <matheeshap62@gmail.com>
2025-12-01 11:01:03 +0530

    Refactors CustomerController to simplify import statements

--- src/main/java/edu/icet/controller/CustomerController.java
+++ src/main/java/edu/icet/controller/CustomerController.java
@@ -3,10 +3,7 @@ package edu.icet.controller;
 import edu.icet.model.entity.Customer;
 import edu.icet.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("customers")
@@ -19,5 +16,4 @@ public class CustomerController {
     public void addCustomer(@RequestBody Customer customer) {
         customerService.add(customer);
     }
-
 }

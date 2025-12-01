3ad8c3e89b0bb6ee5caf50bda973e3aa9759a678
matheeshapr <matheeshap62@gmail.com>
2025-12-01 11:01:12 +0530

    Adds Item entity with attributes and JPA annotations for persistence

--- src/main/java/edu/icet/model/entity/Item.java
+++ src/main/java/edu/icet/model/entity/Item.java
@@ -1,4 +1,21 @@
 package edu.icet.model.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Entity
 public class Item {
+
+    @Id
+    private int code;
+    private String name;
+    private String description;
+    private double unitprice;
+    private int qnt;
 }

aa13e4628e3617bb8398927808907b15df61f001
matheeshapr <matheeshap62@gmail.com>
2025-12-01 11:01:41 +0530

    Defines ItemDTO for `addItem` endpoint and item persistence.

--- src/main/java/edu/icet/model/dto/ItemDTO.java
+++ src/main/java/edu/icet/model/dto/ItemDTO.java
@@ -1,4 +1,17 @@
 package edu.icet.model.dto;
 
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
 public class ItemDTO {
+
+    private int code;
+    private String name;
+    private String description;
+    private double unitprice;
+    private int qnt;
 }

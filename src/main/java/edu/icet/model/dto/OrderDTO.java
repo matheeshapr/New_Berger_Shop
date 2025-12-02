68d301929f348478b2765efb485d0ff701a49643
matheeshapr <matheeshap62@gmail.com>
2025-12-02 17:13:30 +0530

    Enhances OrderDTO with fields and annotations; extends OrderRepository to inherit from JpaRepository

--- src/main/java/edu/icet/model/dto/OrderDTO.java
+++ src/main/java/edu/icet/model/dto/OrderDTO.java
@@ -1,4 +1,17 @@
 package edu.icet.model.dto;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.*;
+
+import java.util.Date;
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class OrderDTO {
+    private int id;
+    private String custid;
+    private String day;
 }
--- src/main/java/edu/icet/repository/OrderRepository.java
+++ src/main/java/edu/icet/repository/OrderRepository.java
@@ -1,4 +1,9 @@
 package edu.icet.repository;
 
-public interface OrderRepository {
+import edu.icet.model.entity.Orders;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrderRepository extends JpaRepository<Orders,Integer> {
 }

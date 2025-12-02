3b4989f71199395c1f419dbde2b8655487f262b1
matheeshapr <matheeshap62@gmail.com>
2025-12-02 17:12:51 +0530

    Removes unused Lombok @Getter annotation from ItemController

--- src/main/java/edu/icet/controller/ItemContoller.java
+++ src/main/java/edu/icet/controller/ItemContoller.java
@@ -2,7 +2,6 @@ package edu.icet.controller;
 
 import edu.icet.model.entity.Item;
 import edu.icet.service.ItemService;
-import lombok.Getter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 

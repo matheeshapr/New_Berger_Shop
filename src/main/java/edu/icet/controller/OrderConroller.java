eb4b2e529f82ffe7dbfec92878150b34d3daae9a
matheeshapr <matheeshap62@gmail.com>
2025-12-02 17:13:13 +0530

    Implements OrderController with addOrder endpoint for order creation

--- src/main/java/edu/icet/controller/OrderConroller.java
+++ src/main/java/edu/icet/controller/OrderConroller.java
@@ -1,4 +1,24 @@
 package edu.icet.controller;
 
+import edu.icet.model.entity.Orders;
+import edu.icet.service.OrderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("orders")
 public class OrderConroller {
+
+    @Autowired
+    OrderService orderService;
+
+    @PostMapping("addorder")
+    public void addOrder(@RequestBody Orders orders){
+        orderService.add(orders);
+    }
+
+//        @GetMapping("getAll")
+//    public String getAll(){
+//        return "Matheesha";
+//    }
 }

1a7047efb76c4279e9118851969491c6cb1f01f3
matheeshapr <matheeshap62@gmail.com>
2025-12-03 08:26:37 +0530

    Defines OrderDetails as JPA entity and removes controller stub.

--- src/main/java/edu/icet/model/entity/OrderDetails.java
+++ src/main/java/edu/icet/model/entity/OrderDetails.java
@@ -1,4 +1,23 @@
 package edu.icet.model.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Entity
 public class OrderDetails {
+
+    @Id
+    private int id;
+    private int itemCode;
+    private int qnty;
+    private double discount;
+
+
+
 }

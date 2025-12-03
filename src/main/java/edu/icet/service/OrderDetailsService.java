49eac1c1d128e58b498b54398c277fee44ad451b
matheeshapr <matheeshap62@gmail.com>
2025-12-03 08:30:56 +0530

    Fleshes out OrderDetailsService to utilize OrderDetailsRepository.

--- src/main/java/edu/icet/service/OrderDetailsService.java
+++ src/main/java/edu/icet/service/OrderDetailsService.java
@@ -1,4 +1,18 @@
 package edu.icet.service;
 
+import edu.icet.controller.OrderDetailsController;
+import edu.icet.model.entity.OrderDetails;
+import edu.icet.repository.OrderDetailsRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class OrderDetailsService {
+
+    @Autowired
+    OrderDetailsRepository orderDetailsRepository;
+
+    public void add(OrderDetails orderDetails) {
+        orderDetailsRepository.save(orderDetails);
+    }
 }

57753971c09e4d65cf8fd87f482e90629a631e4e
matheeshapr <matheeshap62@gmail.com>
2025-12-03 08:30:18 +0530

    Fleshes out OrderDetails DTO, replacing stub for JPA entity.

--- src/main/java/edu/icet/model/dto/OrderDetailsDTO.java
+++ src/main/java/edu/icet/model/dto/OrderDetailsDTO.java
@@ -1,4 +1,16 @@
 package edu.icet.model.dto;
 
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
 public class OrderDetailsDTO {
+
+    private int id;
+    private int itemCode;
+    private int qnty;
+    private double discount;
 }

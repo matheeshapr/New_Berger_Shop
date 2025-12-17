5a9104f9168e4c369625e595e5e5a4fdb2c4614d
matheeshapr <matheeshap62@gmail.com>
2025-12-17 21:45:59 +0530

    Adds customer lookup by name to the CustomerRepository.

--- src/main/java/edu/icet/repository/CustomerRepository.java
+++ src/main/java/edu/icet/repository/CustomerRepository.java
@@ -2,8 +2,15 @@ package edu.icet.repository;
 
 import edu.icet.model.entity.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Repository
 public interface CustomerRepository extends JpaRepository<Customer,Integer> {
+
+    //@Query ("SELECT * FROM Customer WHERE name = :name")
+    Customer findByName(@Param("name") String name);
+
 }

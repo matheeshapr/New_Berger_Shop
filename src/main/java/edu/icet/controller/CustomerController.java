999c4c08cb2b5a02234f698ca130af9353fa3ea0
matheeshapr <matheeshap62@gmail.com>
2026-02-11 14:30:34 +0530

    Configures Customer entity queries, adds search by ID, and cleans paths.

--- src/main/java/edu/icet/controller/CustomerController.java
+++ src/main/java/edu/icet/controller/CustomerController.java
@@ -1,6 +1,5 @@
 package edu.icet.controller;
 
-import edu.icet.model.dto.CustomerDTO;
 import edu.icet.model.entity.Customer;
 import edu.icet.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,33 +8,31 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("customers")
+@RequestMapping("/customers")
 public class CustomerController {
 
     @Autowired
     CustomerService customerService;
 
-    @PostMapping("addcustomer")
+    @PostMapping("/addcustomer")
     public void addCustomer(@RequestBody Customer customer) {
         customerService.add(customer);
     }
 
-    @GetMapping("getcustomer")
+    @GetMapping("/getcustomer")
     public List<Customer> getAll(){
         return customerService.getAllCustomer();
     }
 
-//    @GetMapping("{id}")
-//    public Customer searchById(@PathVariable Integer id){
-//        return customerService.searchStudent(id);
-//    }
+    @GetMapping("/{id}")
+    public Customer searchById(@PathVariable Integer id){
+        return customerService.searchStudent(id);
+    }
 
     @GetMapping("/searchbyname/{name}")
     public List<Customer> searchName(@PathVariable String name){
         return customerService.searchByName(name);
 
     }
-
-
 
 }
